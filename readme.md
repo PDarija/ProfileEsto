ff8567dc540129aedf2a935edb719ab0af09fd80
Darija Paulska <paulskadarija@gmail.com>
2023-04-24 21:52:56 +0300

    #19

--- readme.md
+++ readme.md
@@ -1,20 +1,32 @@
-Check list
+Check list for Authorization https://profile.esto.ee/login/password
 
 
-| No  |                                                                Check name                                                                | Status  | 
-|-----|:----------------------------------------------------------------------------------------------------------------------------------------:|:-------:|
-| 1   |                                              Opportunity to select any authorization method                                              |  Pass   |
-| 2   |                        Additional required fields appear after pressing on methods: Mobile ID, Smart ID, Password                        |  Pass   |
-| 3   |                                       After pressing on "ID card" button, informative text appears                                       |  Pass   |
-| 4   |                                                         Every field is editable                                                          |  Pass   |
-| 5   |                               "This field is required" notification when trying to login with empty fields                               |  Pass   |
-| 6   |                                    When you click on the fields, the color of the frame becomes black                                    |  Pass   |
-| 7   |                                                   Log in button is visible and active                                                    |         |
-| 8   |                                   Login with correct data for Mobile ID method(mobile number, ID code)                                   | Skipped |
-| 9   |                                           Login with correct data for Smart ID method(ID code)                                           | Skipped |
-| 10  |                                     Login with correct data for Password method(Username, Password)                                      | Skipped |
-| 11  |                                                           Login using ID card                                                            | Skipped |
-| 12  |                       "Error! Unknown error 1215" pop-up if login with incorrect credentials for Mobile ID  method                       |         |
-| 13  | "Error!Login failed. Please make sure you are using the latest ID card software" POP-up message if login without card for ID Card method |         |
-| 14  |                    "Username or password is incorrect" message if login with incorrect credentials for login/password                    |         |
-| 15  |                          "Invalid format" message when user incorrect ID code for Smart ID authorization method                          |         |
+| No  |                                         Check name                                         |   Status   | 
+|-----|:------------------------------------------------------------------------------------------:|:----------:|
+| 1   |                       Opportunity to select any authorization method                       |    Pass    |
+| 2   | Additional required fields appear after pressing on methods: Mobile ID, Smart ID, Password |    Pass    |
+| 3   |                After pressing on "ID card" button, informative text appears                |    Pass    |
+| 4   |                                  Every field is editable                                   |    Pass    |
+| 5   |                    Notification when trying to login with empty fields                     |    Pass    |
+| 6   |             When you click on the fields, the color of the frame becomes black             |    Pass    |
+| 7   |                            Log in button is visible and active                             |    Pass    |
+| 8   |            Login with correct data for Mobile ID method(mobile number, ID code)            |  Skipped   |
+| 9   |                    Login with correct data for Smart ID method(ID code)                    |  Skipped   |
+| 10  |              Login with correct data for Password method(Username, Password)               |  Skipped   |
+| 11  |                                    Login using ID card                                     |  Skipped   |
+| 12  |           Error pop-up if login with incorrect credentials for Mobile ID  method           |    Pass    |
+| 13  |               Error POP-up message if login without card for ID Card method                |    Pass    |
+| 14  |            Error message if login with incorrect credentials for login/password            |    Pass    |
+| 15  |     Error message when user enter incorrect ID code for Smart ID authorization method      |    Pass    |
+
+
+Check list for automation tests 
+
+| No  |                             Check name                              |    Status    | 
+|-----|:-------------------------------------------------------------------:|:------------:|
+| 1   |     Every method is visible. User is able to select any method.     |     Pass     |
+| 2   |             Every method have additional fields of text             |     Pass     |
+|     |                                                                     |     Pass     |
+| 4   |         Notification when trying to login with empty fields         |     Pass     |
+| 5   | When you click on the fields, the color of the frame becomes black  |     Pass     |
+| 6   |                 Log in button is visible and active                 |     Pass     |
--- src/test/java/ProfileEsto.java
+++ src/test/java/ProfileEsto.java
@@ -1,71 +1,95 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class ProfileEsto {
+
+    @BeforeEach
+    public void setUp() {
+        open("https://profile.esto.ee/login");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        closeWebDriver();
+    }
 
     @Test
 
 
     public void positiveTestOfAuthorizationMethods() {
         Configuration.holdBrowserOpen = true;
-        open("https://profile.esto.ee/login");
-        $(By.xpath("//a[@data-cy='method-mobile-id']")).shouldBe(Condition.exist);
-        $(By.xpath("//a[@data-cy='method-mobile-id']")).shouldBe(Condition.visible);
-        $(By.xpath("//a[@data-cy='method-mobile-id']")).click();
-        $(By.xpath("//esto-text-input[@data-cy='mobile-id-phone-input']")).shouldBe(Condition.exist);
-        $(By.xpath("//esto-text-input[@data-cy='mobile-id-phone-input']")).shouldBe(Condition.visible);
-        $(By.xpath("//esto-text-input[@data-cy='mobile-id-id-input']")).shouldBe(Condition.exist);
-        $(By.xpath("//esto-text-input[@data-cy='mobile-id-id-input']")).shouldBe(Condition.visible);
+//        open("https://profile.esto.ee/login");
+//        $(By.xpath("//a[@data-cy='method-mobile-id']")).shouldBe(Condition.exist);
+//        $(By.xpath("//a[@data-cy='method-mobile-id']")).shouldBe(Condition.visible);
+//        $(By.xpath("//a[@data-cy='method-mobile-id']")).click();
+//        $(By.xpath("//esto-text-input[@data-cy='mobile-id-phone-input']")).shouldBe(Condition.exist);
+//        $(By.xpath("//esto-text-input[@data-cy='mobile-id-phone-input']")).shouldBe(Condition.visible);
+//        $(By.xpath("//esto-text-input[@data-cy='mobile-id-id-input']")).shouldBe(Condition.exist);
+//        $(By.xpath("//esto-text-input[@data-cy='mobile-id-id-input']")).shouldBe(Condition.visible);
+//        $(By.xpath("//button[@data-cy='mobile-id-login-button']")).shouldBe(Condition.visible);
+//        $(By.xpath("//a[@data-cy='method-smart-id']")).shouldBe(Condition.exist);
+//        $(By.xpath("//a[@data-cy='method-smart-id']")).shouldBe(Condition.visible);
+//        $(By.xpath("//a[@data-cy='method-smart-id']")).click();
+//        $(By.xpath("//esto-text-input[@data-cy='smart-id-input']")).shouldBe(Condition.exist);
+//        $(By.xpath("//esto-text-input[@data-cy='smart-id-input']")).shouldBe(Condition.visible);
+//        $(By.xpath("//button[@data-cy='smart-id-login-button']")).shouldBe(Condition.visible);
+//        $(By.xpath("//a[@data-cy='method-password']")).shouldBe(Condition.exist);
+//        $(By.xpath("//a[@data-cy='method-password']")).shouldBe(Condition.visible);
+//        $(By.xpath("//a[@data-cy='method-password']")).click();
+//        $(By.xpath("//esto-text-input[@data-cy='username-input']")).shouldBe(Condition.exist);
+//        $(By.xpath("//esto-text-input[@data-cy='username-input']")).shouldBe(Condition.visible);
+//        $(By.xpath("//esto-text-input[@data-cy='password-input']")).shouldBe(Condition.exist);
+//        $(By.xpath("//esto-text-input[@data-cy='password-input']")).shouldBe(Condition.visible);
+//        $(By.xpath("//button[@data-cy='password-login-button']")).shouldBe(Condition.visible);
 
-        $(By.xpath("//button[@data-cy='mobile-id-login-button']")).shouldBe(Condition.visible);
-
-
-
-
-        $(By.xpath("//a[@data-cy='method-smart-id']")).shouldBe(Condition.exist);
-        $(By.xpath("//a[@data-cy='method-smart-id']")).shouldBe(Condition.visible);
-        $(By.xpath("//a[@data-cy='method-smart-id']")).click();
-        $(By.xpath("//esto-text-input[@data-cy='smart-id-input']")).shouldBe(Condition.exist);
-        $(By.xpath("//esto-text-input[@data-cy='smart-id-input']")).shouldBe(Condition.visible);
-
-        $(By.xpath("//button[@data-cy='smart-id-login-button']")).shouldBe(Condition.visible);
-
-
-
-        $(By.xpath("//a[@data-cy='method-password']")).shouldBe(Condition.exist);
-        $(By.xpath("//a[@data-cy='method-password']")).shouldBe(Condition.visible);
-        $(By.xpath("//a[@data-cy='method-password']")).click();
-        $(By.xpath("//esto-text-input[@data-cy='username-input']")).shouldBe(Condition.exist);
-        $(By.xpath("//esto-text-input[@data-cy='username-input']")).shouldBe(Condition.visible);
-        $(By.xpath("//esto-text-input[@data-cy='password-input']")).shouldBe(Condition.exist);
-        $(By.xpath("//esto-text-input[@data-cy='password-input']")).shouldBe(Condition.visible);
-
-        $(By.xpath("//button[@data-cy='password-login-button']")).shouldBe(Condition.visible);
+        LoginPage loginPage = new LoginPage();
+        loginPage.checkMobileIdMethodButton();
+        loginPage.clickOnMobileIdButton();
+        loginPage.checkMobileIdPhoneInputField();
+        loginPage.checkMobileIdIdInputField();
+        loginPage.checkMobileIdLoginButton();
+        loginPage.checkSmartIdMethodButton();
+        loginPage.clickSmartIdButton();
+        loginPage.checkSmartIdInputField();
+        loginPage.checkSmartIdLoginButton();
+        loginPage.checkPasswordMethodButton();
+        loginPage.clickPasswordMethodButton();
+        loginPage.checkPasswordMethodUsernameField();
+        loginPage.checkPasswordMethodPasswordField();
+        loginPage.checkPasswordLoginButton();
     }
-    @Test
 
+    @Test
 
     public void negativeTestOfAuthorizationSmartId() {
         Configuration.holdBrowserOpen = true;
-        open("https://profile.esto.ee/login");
-
-        $(By.xpath("//a[@data-cy='method-smart-id']")).shouldBe(Condition.visible);
-        $(By.xpath("//a[@data-cy='method-smart-id']")).click();
-
-        $(By.xpath("//esto-text-input[@data-cy='smart-id-input']")).shouldBe(Condition.visible);
-
-        $(By.xpath("//button[@data-cy='smart-id-login-button']")).shouldBe(Condition.visible);
-        SelenideElement idCodeInput = $(By.xpath("//*[@id=\"login-field\"]/div/input"));
-        idCodeInput.setValue("13465");
+//        open("https://profile.esto.ee/login");
+//        $(By.xpath("//a[@data-cy='method-smart-id']")).shouldBe(Condition.visible);
+//        $(By.xpath("//a[@data-cy='method-smart-id']")).click();
+//        $(By.xpath("//esto-text-input[@data-cy='smart-id-input']")).shouldBe(Condition.visible);
+//        $(By.xpath("//button[@data-cy='smart-id-login-button']")).shouldBe(Condition.visible);
+//        SelenideElement idCodeInput = $(By.xpath("//*[@id='login-field']/div/input"));
+//        idCodeInput.setValue("13465");
         $(By.xpath("//button[@data-cy='smart-id-login-button']")).shouldBe(Condition.visible);
         $(By.xpath("//button[@data-cy='smart-id-login-button']")).click();
         $(By.xpath("//esto-error[@data-cy='smart-id-validation-error']")).shouldBe(Condition.visible);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.checkSmartIdMethodButton();
+        loginPage.clickSmartIdButton();
+        loginPage.checkSmartIdInputField();
+        loginPage.checkSmartIdLoginButton();
+        loginPage.InsertIdCode("255644");
+
+
+
 
     }
 
@@ -77,18 +101,16 @@ public class ProfileEsto {
         $(By.xpath("//a[@data-cy='method-password']")).shouldBe(Condition.visible);
         $(By.xpath("//a[@data-cy='method-password']")).click();
         $(By.xpath("//esto-text-input[@data-cy='username-input']")).shouldBe(Condition.visible);
-        SelenideElement usernameInput = $(By.xpath("//*[@id=\"login-field\"]/div/input"));
+        SelenideElement usernameInput = $(By.xpath("//*[@data-cy='username-input']/div/input"));
         usernameInput.setValue("Test");
         $(By.xpath("//esto-text-input[@data-cy='password-input']")).shouldBe(Condition.visible);
-        SelenideElement usernameInputLogin = $(By.xpath("//*[@id=\"password-field\"]/div/input"));
-        usernameInputLogin.setValue("143qref");
+        SelenideElement usernameInputLogin = $(By.xpath("//*[@data-cy='password-input']/div/input"));
+        usernameInputLogin.setValue("143ref");
         $(By.xpath("//button[@data-cy='password-login-button']")).shouldBe(Condition.visible);
         $(By.xpath("//button[@data-cy='password-login-button']")).click();
 
         $(By.xpath("//span[@data-cy='password-login-error']")).shouldBe(Condition.visible);
     }
-
-
 
 }
 
